5de004c0912e125787e8d29e3fa3083536dcf848
AhmadGafar <ahmed.gafar@pixelogimedia.com>
2021-09-24 22:27:59 +0200

    feat: backend: email post request

--- api/app.js
+++ api/app.js
@@ -7,7 +7,7 @@ var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-var testApiRouter = require("./routes/testApi");
+var emailRouter = require("./routes/email");
 
 var app = express();
 
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/testApi", testApiRouter);
+app.use("/email", emailRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
